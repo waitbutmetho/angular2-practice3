5be48fb3886677e23430dc1af2381e47c02d0b4a
waitbutmetho <silversevenfox@yahoo.com>
2016-04-07 16:36:14 -0700

    create a couple of inputs with examples of databinding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,27 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
+        <h1>My name is {{onTest()}}</h1><br>
+
+        <input class="{{'input1'}}" type="text" value="{{onTest()}}" (keyup)="onKeyup(inputEmlement.value)" #inputEmlement>
+        <p>{{values}}</p>
+        <br>
+        <br>
+        <input type='text' [(ngModel)]="name">
+        <p>Your Name: {{name}}</p>
+      `,
 })
 export class AppComponent {
+  name = 'Josh';
+  values='';
 
+  onTest() {
+    var firstName = 'Josh';
+    var lastName = 'Gustafson';
+    return firstName +" "+ lastName;
+  }
+
+  onKeyup(value: string) {
+    this.values += value + ' | ';
+  }
 }
